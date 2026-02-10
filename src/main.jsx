b82f479747a9ed1d2088a88cad732774f503e635
Cassius Samuel <cassiusejekwu@gmail.com>
2026-02-10 09:11:09 +0100

    removed theme context

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.jsx'
 
-import { ThemeProvider } from './context/ThemeProvider.jsx'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <ThemeProvider>
     <StrictMode>
       <App />
     </StrictMode>
-  </ThemeProvider>
 )
